976e263c710c1652913d70f91cec72ce4f2e7514
Aman Yerpude <aman.yerpude@codenicely.in>
2021-05-10 08:37:53 +0530

    WIP: blog post page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Post from './components/Post'
+import Project from './components/Project'
+import SinglePost from './components/SinglePost'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <section className="App h-screen w-full flex justify-center items-center bg-green-500">
-          <div className="w-full max-w-md bg-gray-800" >
-            <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-              <div className="px-4 pb-4">
-                <label htmlFor="email" className="text-sm block font-bold  pb-2">EMAIL ADDRESS</label>
-                <input type="email" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="Johnbull@example.com" />
-              </div>
-              <div className="px-4 pb-4">
-                <label htmlFor="password" className="text-sm block font-bold pb-2">PASSWORD</label>
-                <input type="password" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password" />
-              </div>
-              <div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
+        <Route component={SinglePost} path='/post/:slug' />
+        <Route component={Post} path='/post' />
+        <Route component={Project} path='/project' />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
